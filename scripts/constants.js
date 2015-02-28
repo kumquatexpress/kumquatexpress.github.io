c5d2af221ff5cc255248924a00eedac82945ca3e
Boyang Niu <boyang.niu@gmail.com>
2015-02-27 23:50:22 -0500

    Add Vroom to projects

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,27 +1,30 @@
 var constants = {
-	image_text: {
-		maas: {
-			text: "<p><h5>MAAS - Metal As a Service</h5>An open source server management tool from Canonical. Images, reboots, and registers cloud and baremetal servers while displaying a clean and user-friendly UI. Implemented an extension for custom image use at Ooyala. Made using Python/Django, Summer 2013.</p>"
-		},
-		codify: {
-			text: "<p><h5>Codify</h5>Hackathon project from PennApps Fall 2012. Offers a collaborative, real-time environment for designing and visualizing object-oriented systems using Node.js. Also has some scaffolding functionality for certain languages.</p>",
-		},
-		visualizer: {
-			text: "<p><h5>Visualizer</h5>A fun side project using the Echo Nest and SoundCloud APIs to analyze songs and visualize them according to the pitch and rhythm of each beat. Entirely in Javascript, Winter 2013.</p>",
-		},
-		venn: {
-			text: "<p><h5>Venn</h5>Social network for forming platonic relationships. Uses a combination of Facebook user data and crowdsourced answers to questions to match people. Quizlet interface and basic ML over profiles implemented from scratch. Built with MongoDB and Flask, Fall 2014.</p>"
-		},
-		clairvoyance: {
-			text: "<p><h5>Clairvoyance</h5>A team composition analyzer and synergy estimator for League of Legends. By analyzing the Riot API, gives any number of champions an estimated win rate and compatibility rating. Also able to pull live match data straight from the servers using RTMP. Built with Sinatra and Postgres, Spring 2014.</p>"
-		},
-		wordcraft: {
-			text: "<p><h5>WordCraft</h5>My first ridiculous attempt at making a game using Unity. All models, scripts, and music are original. The gameplay was meant to be a cross between Scrabble and Space Invaders, where letters drop from the sky and points are scored by spelling words. Check it out on Kongregate! Spring 2013.</p>"
-		}
-	},
-	urls: {
-		github: "http://www.github.com/kumquatexpress",
-		linkedin: "http://www.linkedin.com/profile/view?id=159503346",
-		facebook: "http://www.facebook.com/boyang.niu",
-	}
+    image_text: {
+        maas: {
+            text: "<p><h5>MAAS - Metal As a Service</h5>An open source server management tool from Canonical. Images, reboots, and registers cloud and baremetal servers while displaying a clean and user-friendly UI. Implemented an extension for custom image use at Ooyala. Made using Python/Django, Summer 2013.</p>"
+        },
+        codify: {
+            text: "<p><h5>Codify</h5>Hackathon project from PennApps Fall 2012. Offers a collaborative, real-time environment for designing and visualizing object-oriented systems using Node.js. Also has some scaffolding functionality for certain languages.</p>",
+        },
+        visualizer: {
+            text: "<p><h5>Visualizer</h5>A fun side project using the Echo Nest and SoundCloud APIs to analyze songs and visualize them according to the pitch and rhythm of each beat. Entirely in Javascript, Winter 2013.</p>",
+        },
+        venn: {
+            text: "<p><h5>Venn</h5>Social network for forming platonic relationships. Uses a combination of Facebook user data and crowdsourced answers to questions to match people. Quizlet interface and basic ML over profiles implemented from scratch. Built with MongoDB and Flask, Fall 2014.</p>"
+        },
+        clairvoyance: {
+            text: "<p><h5>Clairvoyance</h5>A team composition analyzer and synergy estimator for League of Legends. By analyzing the Riot API, gives any number of champions an estimated win rate and compatibility rating. Also able to pull live match data straight from the servers using RTMP. Built with Sinatra and Postgres, Spring 2014.</p>"
+        },
+        wordcraft: {
+            text: "<p><h5>WordCraft</h5>My first ridiculous attempt at making a game using Unity. All models, scripts, and music are original. The gameplay was meant to be a cross between Scrabble and Space Invaders, where letters drop from the sky and points are scored by spelling words. Check it out on Kongregate! Spring 2013.</p>"
+        },
+        vroom: {
+            text: "<p><h5>Vroom</h5>A static website generator and framework written in Golang and using the Go template engine. Built because I wanted to make a Golang solution for something as flexible and useful as Jekyll. Open sourced and in use on Github! Spring 2015.</p>"
+        },
+    },
+    urls: {
+        github: "http://www.github.com/kumquatexpress",
+        linkedin: "http://www.linkedin.com/profile/view?id=159503346",
+        facebook: "http://www.facebook.com/boyang.niu",
+    }
 };
